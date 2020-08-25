7ce88b60a733d7626d73ece80c9367d1e94c4afd
aibr24 <aliibrahim4793@gmail.com>
2020-08-25 21:06:18 +0300

    cleanup

--- components/TripList/index.js
+++ components/TripList/index.js
@@ -4,7 +4,7 @@ import tripStore from "../../stores/tripStore";
 import TripItem from "./TripItem";
 import { Content, List } from "native-base";
 import { observer } from "mobx-react";
-import trip from "../../data";
+
 const TripList = () => {
   //
   const listoftrips = tripStore.trips.map((item) => (
