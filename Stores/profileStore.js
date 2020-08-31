d37c257d5914cd3f7e34bb696f2d3fb8a02276cd
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-08-31 18:03:27 +0300

    AllTrips component, profileStore(fetch and update)

---
+++ Stores/profileStore.js
@@ -0,0 +1,36 @@
+import { decorate, observable } from "mobx";
+import instance from "./instance";
+
+class ProfileStore {
+  profiles = [];
+
+  fetchProfiles = async () => {
+    try {
+      const res = await instance.get("/profiles");
+      this.profiles = res.data;
+    } catch (error) {
+      console.log("TRIP-STORE >> fetchProfiles() --->", error);
+    }
+  };
+
+  updateProfile = async (oldProfile) => {
+    try {
+      await instance.put(`/profiles/${oldProfile.id}`, oldProfile);
+      const foundProfile = this.profiles.find(
+        (profile) => profile.id === oldProfile.id
+      );
+      for (const key in oldProfile) foundProfile[key] = oldProfile[key];
+    } catch (error) {
+      console.log("TRIP-STORE >> updateProfile() --->", error);
+    }
+  };
+}
+
+decorate(ProfileStore, {
+  profiles: observable,
+});
+
+const profileStore = new ProfileStore();
+profileStore.fetchProfiles();
+
+export default profileStore;
