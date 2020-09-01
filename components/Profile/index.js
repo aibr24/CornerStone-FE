a0d0d48628bacc90073686c6137a231fad96751f
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-09-01 19:18:06 +0300

    profile

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,85 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { observer } from "mobx-react";
+// Components
+import TripItem from "../TripList/TripItem";
 
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import MaterialIcons from "react-native-vector-icons/MaterialCommunityIcons";
+// Styles
+import { View } from "react-native";
+
 import Ionicons from "react-native-vector-icons/Ionicons";
-
 import {
   ProfileContainer,
   ProfileImage,
   ImageStyled,
-  DmStyled,
   ActiveStyled,
   AddStyled,
   InfoContainer,
   TextStyled,
   StatsContainerStyled,
   StatsBoxStyled,
-  MediaImageContainer,
-  MediaCount,
-  SubTextRecent,
-  RecentItemStyled,
-  ActivityIdicatorStyled,
 } from "./styles";
 
+// Stores
+import profileStore from "../../stores/profileStore";
+import authStore from "../../stores/authStore";
+import tripStore from "../../stores/tripStore";
+
 const Profile = () => {
+  let profile = null;
+  if (authStore.user)
+    profile = profileStore.profiles.find(
+      (item) => item.userId === authStore.user.id
+    );
+  const listOfTrips = tripStore.trips.filter(
+    (item) => item.userId == authStore.user.id
+  );
+
+  const tripSum = () => listOfTrips.length;
+
   return (
     <ProfileContainer>
-      <ScrollView showsHorizontalScrollIndicator={false}>
-        <Text>Hello there!</Text>
-        <View style={{ alignSelf: "center" }}>
-          <ProfileImage>
-            <ImageStyled
-              source={{
-                uri:
-                  "https://rakshasa.me/wp-content/uploads/avatars/6473/5bbea254b5f46-bpfull.png",
-              }}
-              // resizeMode="center" -> this was used in the original code but when I use it, it gives me a square image
-            ></ImageStyled>
-          </ProfileImage>
-          <DmStyled>
-            <MaterialIcons
-              name="chat"
-              size={18}
-              color="#DFD8C8"
-            ></MaterialIcons>
-          </DmStyled>
-          <ActiveStyled></ActiveStyled>
-          <AddStyled>
-            <Ionicons
-              name="ios-add"
-              size={48}
-              color="#DFD8C8"
-              style={{ marginTop: 6, marginLeft: 2 }}
-            ></Ionicons>
-          </AddStyled>
-        </View>
+      <View style={{ alignSelf: "center" }}>
+        <ProfileImage>
+          <ImageStyled
+            source={{
+              uri: profile.image
+                ? profile.image
+                : "https://pwcenter.org/sites/default/files/default_images/default_profile.png",
+            }}
+          ></ImageStyled>
+        </ProfileImage>
+        <ActiveStyled></ActiveStyled>
+        <AddStyled>
+          <Ionicons
+            name="ios-add"
+            size={48}
+            color="#DFD8C8"
+            style={{ marginTop: 6, marginLeft: 2 }}
+          ></Ionicons>
+        </AddStyled>
+      </View>
 
-        <InfoContainer>
-          <TextStyled style={{ fontWeight: "200", fontSize: 36 }}>
-            Madara
-          </TextStyled>
-          <TextStyled style={{ color: "#AEB5BC", fontSize: 14 }}>
-            Photograph
-          </TextStyled>
-        </InfoContainer>
+      <InfoContainer>
+        <TextStyled style={{ fontWeight: "200", fontSize: 36 }}>
+          {authStore.user.username}
+        </TextStyled>
+        <TextStyled style={{ color: "#AEB5BC", fontSize: 14 }}>
+          {profile.bio ? profile.bio : "no bio available"}
+        </TextStyled>
+      </InfoContainer>
 
-        <StatsContainerStyled>
-          <StatsBoxStyled>
-            <TextStyled style={{ fontSize: 24 }}>434</TextStyled>
-            <TextStyled className="subText">Posts</TextStyled>
-          </StatsBoxStyled>
-          <StatsBoxStyled>
-            <TextStyled style={{ fontSize: 24 }}>1,000,000</TextStyled>
-            <TextStyled className="subText">Followers</TextStyled>
-          </StatsBoxStyled>
-          <StatsBoxStyled>
-            <TextStyled style={{ fontSize: 24 }}>144</TextStyled>
-            <TextStyled className="subText">Following</TextStyled>
-          </StatsBoxStyled>
-        </StatsContainerStyled>
+      <StatsContainerStyled>
+        <StatsBoxStyled>
+          <TextStyled style={{ fontSize: 24 }}>{() => tripSum}</TextStyled>
+          <TextStyled className="subText">My Trips</TextStyled>
+        </StatsBoxStyled>
+      </StatsContainerStyled>
 
-        <View style={{ marginTop: 32 }}>
+      {/* <View style={{ marginTop: 32 }}>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
             <MediaImageContainer>
               <ImageStyled
@@ -125,32 +121,9 @@ const Profile = () => {
               Media
             </TextStyled>
           </MediaCount>
-        </View>
-        <SubTextRecent>Recent Activity</SubTextRecent>
-        <View style={{ alignItems: "center" }}>
-          <RecentItemStyled>
-            <ActivityIdicatorStyled></ActivityIdicatorStyled>
-            <View style={{ width: 250 }}>
-              <TextStyled style={{ color: "#41444B", fontWeight: "300" }}>
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and{" "}
-                <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
-              </TextStyled>
-            </View>
-          </RecentItemStyled>
-          <RecentItemStyled>
-            <ActivityIdicatorStyled></ActivityIdicatorStyled>
-            <View style={{ width: 250 }}>
-              <TextStyled style={{ color: "#41444B", fontWeight: "300" }}>
-                Started following{" "}
-                <Text style={{ fontWeight: "400" }}>Luke Harper</Text>
-              </TextStyled>
-            </View>
-          </RecentItemStyled>
-        </View>
-      </ScrollView>
+        </View> */}
     </ProfileContainer>
   );
 };
 
-export default Profile;
+export default observer(Profile);
